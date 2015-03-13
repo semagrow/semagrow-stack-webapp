a13bc11dca4a497864d1d65ff9477c3cbe356788
Giannis Mouchakis <gmouchakis@iit.demokritos.gr>
2015-03-13 15:12:05 +0200

    first version of handling

--- src/main/java/eu/semagrow/stack/webapp/controllers/SparqlController.java
+++ src/main/java/eu/semagrow/stack/webapp/controllers/SparqlController.java
@@ -1,48 +1,45 @@
 package eu.semagrow.stack.webapp.controllers;
 
-import eu.semagrow.modules.fileutils.FileUtils;
-import eu.semagrow.modules.sparqlutils.SparqlUtils;
-import eu.semagrow.stack.modules.api.decomposer.QueryDecompositionException;
-import eu.semagrow.stack.modules.api.query.SemagrowTupleQuery;
-import eu.semagrow.stack.modules.api.repository.SemagrowRepository;
-import eu.semagrow.stack.modules.commons.CONSTANTS;
-
-import java.io.*;
-import java.net.URL;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import eu.semagrow.stack.modules.sails.semagrow.config.SemagrowRepositoryConfig;
-import eu.semagrow.stack.webapp.controllers.exceptions.SemaGrowInternalException;
-
 import org.openrdf.model.Graph;
-import org.openrdf.model.Resource;
 import org.openrdf.model.impl.GraphImpl;
 import org.openrdf.query.BooleanQuery;
 import org.openrdf.query.GraphQuery;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.Query;
 import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryInterruptedException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResultHandlerException;
 import org.openrdf.query.UpdateExecutionException;
 import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.algebra.evaluation.ValueExprEvaluationException;
 import org.openrdf.query.parser.ParsedOperation;
 import org.openrdf.query.parser.ParsedQuery;
 import org.openrdf.query.parser.ParsedUpdate;
 import org.openrdf.query.parser.QueryParserUtil;
 import org.openrdf.query.resultio.sparqljson.SPARQLResultsJSONWriter;
 import org.openrdf.query.resultio.sparqlxml.SPARQLResultsXMLWriter;
-import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.config.*;
+import org.openrdf.repository.config.RepositoryConfig;
+import org.openrdf.repository.config.RepositoryConfigException;
+import org.openrdf.repository.config.RepositoryFactory;
+import org.openrdf.repository.config.RepositoryImplConfig;
+import org.openrdf.repository.config.RepositoryRegistry;
 import org.openrdf.repository.http.HTTPQueryEvaluationException;
-import org.openrdf.repository.sail.config.SailRepositoryConfig;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParser;
@@ -54,11 +51,21 @@ import org.openrdf.rio.trig.TriGWriter;
 import org.openrdf.rio.trix.TriXWriter;
 import org.openrdf.rio.turtle.TurtleWriter;
 import org.openrdf.sail.config.SailConfigException;
+import org.openrdf.sail.rdbms.exceptions.RdbmsQueryEvaluationException;
+import org.openrdf.sail.rdbms.exceptions.UnsupportedRdbmsOperatorException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import eu.semagrow.modules.fileutils.FileUtils;
+import eu.semagrow.modules.sparqlutils.SparqlUtils;
+import eu.semagrow.stack.modules.api.decomposer.QueryDecompositionException;
+import eu.semagrow.stack.modules.api.query.SemagrowTupleQuery;
+import eu.semagrow.stack.modules.api.repository.SemagrowRepository;
+import eu.semagrow.stack.modules.commons.CONSTANTS;
+import eu.semagrow.stack.modules.sails.semagrow.config.SemagrowRepositoryConfig;
 
 /**
  *
@@ -168,7 +175,7 @@ public class SparqlController {
                 String acceptFormat = !accept.equals("")?accept:request.getHeader("accept");
                 accept = SparqlUtils.getAcceptMimeType(q, acceptFormat);
                 response.setContentType(accept);
-                this.handleQuery(response.getOutputStream(), accept, q);
+                this.handleQuery(response, accept, q);
             }            
         } finally {
             if(repCon!=null){
@@ -253,72 +260,68 @@ public class SparqlController {
         }
     }    
     
-    private void handleQuery(OutputStream out, String accept, Query query) 
-            throws Exception {
-            if(query instanceof TupleQuery){
-            	try {
-	                if(accept.indexOf(CONSTANTS.MIMETYPES.SPARQLRESULTS_XML)!=-1){
-	                    ((TupleQuery)query).evaluate(new SPARQLResultsXMLWriter(out));
-	                } else
-	                if(accept.indexOf(CONSTANTS.MIMETYPES.SPARQLRESULTS_JSON)!=-1){
-	                    ((TupleQuery)query).evaluate(new SPARQLResultsJSONWriter(out));
-	                } else {
-	                    ((TupleQuery)query).evaluate(new eu.semagrow.modules.rioutils.queryresultio.HTMLTableWriter(out));
-	                }
-            	} catch (TupleQueryResultHandlerException e) {
-        			
-        		} catch (QueryEvaluationException e) {
-        			
-        		} 
-            } else if(query instanceof GraphQuery){
-            	try {
-	                if(accept.indexOf(CONSTANTS.MIMETYPES.RDF_RDFXML)!=-1) {
-	                    ((GraphQuery)query).evaluate(new RDFXMLPrettyWriter(out));
-	                } else
-	                if(accept.indexOf(CONSTANTS.MIMETYPES.RDF_N3)!=-1) {
-	                    ((GraphQuery)query).evaluate(new N3Writer(out));
-	                } else
-	                if(accept.indexOf(CONSTANTS.MIMETYPES.RDF_TURTLE)!=-1) {
-	                    ((GraphQuery)query).evaluate(new TurtleWriter(out));
-	                } else
-	                if(accept.indexOf(CONSTANTS.MIMETYPES.RDF_TRIG)!=-1) {
-	                    ((GraphQuery)query).evaluate(new TriGWriter(out));
-	                } else
-	                if(accept.indexOf(CONSTANTS.MIMETYPES.RDF_TRIX)!=-1) {
-	                    ((GraphQuery)query).evaluate(new TriXWriter(out));
-	                } else {
-	                    ((GraphQuery)query).evaluate(new TurtleWriter(out));
-	                }    
-            	} catch (RDFHandlerException e) {
-            		// These exceptions are internal to the SemaGrow Stack
-        			throw new Exception( e );
-	    		} catch (QueryEvaluationException e) {
-	    			if( (e.getCause() != null) && (e.getCause() instanceof IOException) ) {
-	            		// These exceptions are caused by inability to feed the OutputStream with
-	    				// the response, and are internal to the SemaGrow Stack
-	    				throw new SemaGrowInternalException("SemaGrow internal exception", e.getCause());
-	    			}
-	    			else {
-	            		// These exceptions are caused by inability to get results from the federated
-	    				// endpoints and are external to the SemaGrow Stack
-	    				// XXX:NOTE: MUST CHECK FOR OTHER causes that are actually internal as immediatelly above
-	    				
-	    			}
-	    		} 
-
-            } else if(query instanceof BooleanQuery) {
-            	// This is the only accept branch where IOException is not encapsulated
-            	// inside QueryEvaluationException. This IOException should be treated
-            	// uniformly as with QueryEvaluationExceptionS caused by IOExceptionS
-            	// above.
-                try {
-					out.write((((BooleanQuery)query).evaluate()+"").getBytes());
-				} catch (IOException e) {
-					throw new QueryEvaluationException(e.getMessage(), e);
-				} catch (QueryEvaluationException e) {
-			
+    private void handleQuery(HttpServletResponse response, String accept, Query query) 
+    		throws IOException, TupleQueryResultHandlerException, QueryEvaluationException, RDFHandlerException  {
+    	OutputStream out = response.getOutputStream();
+    	try {
+			if (query instanceof TupleQuery) {
+				if (accept.indexOf(CONSTANTS.MIMETYPES.SPARQLRESULTS_XML) != -1) {
+					((TupleQuery) query).evaluate(new SPARQLResultsXMLWriter(out));
+				} else if (accept.indexOf(CONSTANTS.MIMETYPES.SPARQLRESULTS_JSON) != -1) {
+					((TupleQuery) query).evaluate(new SPARQLResultsJSONWriter(out));
+				} else {
+					((TupleQuery) query).evaluate(new eu.semagrow.modules.rioutils.queryresultio.HTMLTableWriter(out));
 				}
-            }
-    	
+			} else if (query instanceof GraphQuery) {
+				if (accept.indexOf(CONSTANTS.MIMETYPES.RDF_RDFXML) != -1) {
+					((GraphQuery) query).evaluate(new RDFXMLPrettyWriter(out));
+				} else if (accept.indexOf(CONSTANTS.MIMETYPES.RDF_N3) != -1) {
+					((GraphQuery) query).evaluate(new N3Writer(out));
+				} else if (accept.indexOf(CONSTANTS.MIMETYPES.RDF_TURTLE) != -1) {
+					((GraphQuery) query).evaluate(new TurtleWriter(out));
+				} else if (accept.indexOf(CONSTANTS.MIMETYPES.RDF_TRIG) != -1) {
+					((GraphQuery) query).evaluate(new TriGWriter(out));
+				} else if (accept.indexOf(CONSTANTS.MIMETYPES.RDF_TRIX) != -1) {
+					((GraphQuery) query).evaluate(new TriXWriter(out));
+				} else {
+					((GraphQuery) query).evaluate(new TurtleWriter(out));
+				}
+			} else if (query instanceof BooleanQuery) {
+				out.write((((BooleanQuery) query).evaluate() + "").getBytes());
+			}
+    	} catch (TupleQueryResultHandlerException e) {
+    		response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+    		out.write(Arrays.toString(e.getStackTrace()).getBytes());
+			throw e;
+		} catch (QueryEvaluationException e) {
+			if (e instanceof QueryInterruptedException) {
+				response.setStatus(HttpServletResponse.SC_GATEWAY_TIMEOUT);			
+			} else if (e instanceof HTTPQueryEvaluationException) {
+				if (((HTTPQueryEvaluationException) e).isCausedByIOException()) {
+					response.setStatus(HttpServletResponse.SC_GATEWAY_TIMEOUT);
+				} else if (((HTTPQueryEvaluationException) e).isCausedByMalformedQueryException()) {
+					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				} else if (((HTTPQueryEvaluationException) e).isCausedByRepositoryException()) {
+					response.setStatus(HttpServletResponse.SC_GATEWAY_TIMEOUT);
+				} else {
+					response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				}
+			} else if (e instanceof RdbmsQueryEvaluationException) {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			} else if (e instanceof UnsupportedRdbmsOperatorException) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			} else if (e instanceof ValueExprEvaluationException) {
+				response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+			} else {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+			out.write(Arrays.toString(e.getStackTrace()).getBytes());
+			throw e;
+		} catch (RDFHandlerException e) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			out.write(Arrays.toString(e.getStackTrace()).getBytes());
+			throw e;
+		}
+ 	
     }    
 }
